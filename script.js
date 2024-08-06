58573320f7fa7c4afe4f14b200798929a9cd5b4f
Daniel Gal <danihaif@gmail.com>
2024-08-06 13:50:17 +0300

    responsive

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("JavaScript loaded");
     const hamburger = document.querySelector(".hamburger");
-    const nav = document.querySelector("nav");
+    const headerContainer = document.querySelector(".header-container");
+    const navMenu = document.querySelector("nav");
     const backdrop = document.querySelector(".backdrop");
-    const navLinks = document.querySelectorAll("nav ul li a");
 
     hamburger.addEventListener("click", function () {
-        nav.classList.toggle("show");
+        navMenu.classList.toggle("show");
         backdrop.classList.toggle("show");
     });
 
     backdrop.addEventListener("click", function () {
-        nav.classList.remove("show");
+        navMenu.classList.remove("show");
         backdrop.classList.remove("show");
-    });
-
-    navLinks.forEach(link => {
-        link.addEventListener("click", function () {
-            nav.classList.remove("show");
-            backdrop.classList.remove("show");
-        });
     });
 });
