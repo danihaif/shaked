e0cf656a946b6d6557c7ef5c0f9a4e58da29f8e8
Daniel Gal <danihaif@gmail.com>
2024-08-06 14:12:41 +0300

    contact

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("JavaScript loaded");
     const hamburger = document.querySelector(".hamburger");
-    const headerContainer = document.querySelector(".header-container");
-    const navMenu = document.querySelector("nav");
+    const nav = document.querySelector("nav");
     const backdrop = document.querySelector(".backdrop");
+    const navLinks = document.querySelectorAll("nav ul li a");
 
     hamburger.addEventListener("click", function () {
-        navMenu.classList.toggle("show");
+        nav.classList.toggle("show");
         backdrop.classList.toggle("show");
     });
 
     backdrop.addEventListener("click", function () {
-        navMenu.classList.remove("show");
+        nav.classList.remove("show");
         backdrop.classList.remove("show");
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            nav.classList.remove("show");
+            backdrop.classList.remove("show");
+        });
     });
 });
